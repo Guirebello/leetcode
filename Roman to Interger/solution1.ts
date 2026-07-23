f25cda5d86b65135da3d044dd816a7c84245ef18
Guilherme Rebello <gui.rebello1@gmail.com>
2026-07-23 18:53:46 -0300

    wip solution

---
+++ Roman to Interger/solution1.ts
@@ -0,0 +1,43 @@
+// function romanToInt(s: string): number {
+//   const romanMap = new Map([
+//     ['I', 1],
+//     ['V', 5],
+//     ['X', 10],
+//     ['L', 50],
+//     ['C', 100],
+//     ['D', 500],
+//     ['M', 1000],
+//   ])
+//   const romanArray = s.split('')
+//   const romanToNumberArray = romanArray.map((n) => romanMap.get(n) ?? 0)
+//   const result = romanToNumberArray.reduce((acc, n) => {
+//     return acc + n
+//   }, 0)
+//   return result
+// }
+
+function romanToInt(s: string): number {
+  const romanMap = new Map([
+    ['I', 1],
+    ['V', 5],
+    ['X', 10],
+    ['L', 50],
+    ['C', 100],
+    ['D', 500],
+    ['M', 1000],
+  ])
+  const romanArray = s.split('')
+
+  // IV and IX (4, 9)
+  // XL and XC (40, 90)
+  // CD and CM (400, 900)
+  for (let i = 0; i < romanArray.length; i++) {
+    if (i > 0 && romanArray[i - 1] == 'I') {
+    }
+  }
+
+  const result = romanToNumberArray.reduce((acc, n) => {
+    return acc + n
+  }, 0)
+  return result
+}
